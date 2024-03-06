3070cca3a2285ece03cc2b4cbd74c27c3faf6707
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2024-03-06 19:11:10 +0100

    Add main page

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,15 @@
+import { PropsWithChildren } from "react";
+import { Navbar } from "@/app/(marketing)/_components/navbar";
+import { Footer } from "@/app/(marketing)/_components/footer";
+
+export default function MarketingLayout({ children }: PropsWithChildren) {
+    return (
+        <div className="h-full bg-slate-100">
+            <Navbar/>
+            <main className="pt-40 pb-20 bg-slate-100">
+                {children}
+            </main>
+            <Footer/>
+        </div>
+    );
+}
