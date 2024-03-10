56d7628db9a65f5ba2f21d3d12a7383ecf130e05
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2024-03-10 10:52:05 +0100

    Add card actions

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -6,6 +6,8 @@ import { useQuery } from "@tanstack/react-query";
 import { CardWithList } from "@/types";
 import { fetcher } from "@/lib/fetcher";
 import { Header } from "@/components/modals/card-modal/header";
+import { Description } from "@/components/modals/card-modal/description";
+import { Actions } from "@/components/modals/card-modal/actions";
 
 export function CardModal() {
     const id = useCardModal((state) => state.id);
@@ -24,6 +26,14 @@ export function CardModal() {
         >
             <DialogContent>
                 {!cardData ? <Header.Skeleton/> : <Header data={cardData}/>}
+                <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
+                    <div className="col-span-3">
+                        <div className="w-full space-y-6">
+                            {!cardData ? <Description.Skeleton/> : <Description data={cardData}/>}
+                        </div>
+                    </div>
+                    {!cardData ? <Actions.Skeleton/> : <Actions data={cardData}/>}
+                </div>
             </DialogContent>
         </Dialog>
     );
